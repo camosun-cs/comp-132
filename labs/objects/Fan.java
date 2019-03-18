be4f5b2f62e21c9d3811d916e4196b949c723718
shohreh-hadian <shohreh.hadian@gmail.com>
2019-03-18 00:33:14 -0700

    Added Polygon images and made minor edits to the index file.

---
+++ labs/objects/Fan.java
@@ -0,0 +1,55 @@
+package DemoClass.InClassActivity;
+
+class Fan {
+  public static int SLOW = 1;
+  public static int MEDIUM = 2;
+  public static int FAST = 3;
+
+  private int speed = SLOW;
+  private boolean on = false;
+  private double radius = 5;
+  private String color = "white";
+
+  public Fan() {
+  }
+
+  public int getSpeed() {
+    return speed;
+  }
+
+  public void setSpeed(int newSpeed) {
+    speed = newSpeed;
+  }
+
+  public boolean isOn() {
+    return on;
+  }
+
+  public void setOn(boolean trueOrFalse) {
+    this.on = trueOrFalse;
+  }
+
+  public double getRadius() {
+    return radius;
+  }
+
+  public void setRadius(double newRadius) {
+    radius = newRadius;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String newColor) {
+    color = newColor;
+  }
+
+  @Override
+  public String toString() {
+    return "speed " + speed + "\n"
+      + "color " + color + "\n"
+      + "radius " + radius + "\n"
+      + ((on) ? "fan is on" : " fan is off");
+  }
+}// end of class Fan
