b485ebcf338aad9fc04977c650bc4ccd2ef9d3b9
Maxwell Terpstra <telic.detour+github@gmail.com>
2020-07-02 11:11:26 -0700

    remove COMP139 and other unused content

--- labs/arrays/input-snippet.java
+++
@@ -1,8 +0,0 @@
-/* Snippet of the code for inputting data */
-
-Scanner userInputScanner = new Scanner(System.in);
-char inputFromUser;
-String inputFromUser_string;
-System.out.println("Please Enter the next Character");
-inputFromUser_string = userInputScanner.next();
-inputFromUser = inputFromUser_string.charAt(0);
